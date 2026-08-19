deb51f39815f7c7efa601512b2a4c6ada82edfa4
Muhsiny <138768284+Muhsiny@users.noreply.github.com>
2026-08-19 23:53:40 +0430

    Add shared core and Windows desktop modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,5 @@ pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal()
 dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
 rootProject.name = "WifiRouterControl"
 include(":app")
+include(":core")
+include(":desktop")
