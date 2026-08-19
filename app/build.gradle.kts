08ae23fa0bc21f9371b056b840096ad2f0135f8a
Muhsiny <138768284+Muhsiny@users.noreply.github.com>
2026-08-20 00:57:41 +0430

    Bump WiFi Control to v3.1 direct endpoint engine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
     namespace = "org.sayeh.wificontrol"
     compileSdk = 35
-    defaultConfig { applicationId = "org.sayeh.wificontrol"; minSdk = 23; targetSdk = 35; versionCode = 30; versionName = "3.0.0" }
+    defaultConfig { applicationId = "org.sayeh.wificontrol"; minSdk = 23; targetSdk = 35; versionCode = 31; versionName = "3.1.0" }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_1_8; targetCompatibility = JavaVersion.VERSION_1_8 }
     kotlinOptions { jvmTarget = "1.8" }
 }
