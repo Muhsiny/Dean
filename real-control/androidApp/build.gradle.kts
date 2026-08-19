a4d86545d188ffe693b0f826432de7ebc0df624b
Muhsiny <138768284+Muhsiny@users.noreply.github.com>
2026-08-20 01:54:38 +0430

    Add Android app module

---
+++ real-control/androidApp/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+
+android {
+    namespace = "org.sayeh.realwifi"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "org.sayeh.realwifi"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0-real"
+    }
+    compileOptions { sourceCompatibility = JavaVersion.VERSION_1_8; targetCompatibility = JavaVersion.VERSION_1_8 }
+    kotlinOptions { jvmTarget = "1.8" }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+}
