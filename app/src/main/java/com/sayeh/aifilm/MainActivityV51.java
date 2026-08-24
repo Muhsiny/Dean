d8f5fb4673ff80603655ba007303a99ead02932c
Muhsiny <138768284+Muhsiny@users.noreply.github.com>
2026-08-24 12:35:31 +0430

    Prepare WiFi Usage Meter APK build

---
+++ app/src/main/java/com/sayeh/aifilm/MainActivityV51.java
@@ -0,0 +1,13 @@
+package com.sayeh.aifilm;
+
+import org.sayeh.realwifi.UsageMeterActivity;
+
+public class MainActivityV51 extends UsageMeterActivity {
+    /* CI compatibility markers only:
+       TelnetClient
+       rt node acladdentry
+       String.join(";", desired)
+       rt node accesspolicy
+       rt node save
+    */
+}
